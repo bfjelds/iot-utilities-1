a4136a12764e579a745bbbccd70cbce367c370a7
Rachit Bajpai <rachitb@microsoft.com>
2015-09-02 11:11:18 -0700

    Initial implementation for device center

---
+++ DeviceCenter/DeviceCenter/SetupDevicePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeviceCenter
+{
+    /// <summary>
+    /// Interaction logic for SetupDevicePage.xaml
+    /// </summary>
+    public partial class SetupDevicePage : Page
+    {
+        public SetupDevicePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshDriveList();
+        }
+
+        private void FlashSDCard_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorMessage.Visibility = Visibility.Visible; 
+        }
+
+        private void RefreshDriveList()
+        {
+            var drives = DriveInfo.GetRemovableDriveList();
+            RemoveableDevicesComboBox.Items.Clear();
+            
+            if (drives.Count == 0)
+            {
+                StatusMessageBox.Text = "No SD cards found. Please insert one and press the refresh button.";
+                var item = new ListBoxItem();
+                item.Content = "No SD cards found";
+                RemoveableDevicesComboBox.Items.Add(item);
+                RemoveableDevicesComboBox.SelectedIndex = -1;
+                RemoveableDevicesComboBox.IsEnabled = false;
+            }
+            else
+            {
+                foreach (var drive in drives)
+                {
+                    StatusMessageBox.Text = "";
+                    var item = new ListBoxItem();
+                    item.Content = String.Format("{0} {1} [{2}]", drive.DriveName, drive.SizeString, drive.Model);
+                    item.Tag = drive;
+                    RemoveableDevicesComboBox.Items.Add(item);
+                    RemoveableDevicesComboBox.SelectedIndex = 0;
+                    RemoveableDevicesComboBox.IsEnabled = true;
+                }
+            }
+        }
+
+        private void btnContinue_Click(object sender, RoutedEventArgs e)
+        {
+            // Do the actual flashing here 
+           
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorMessage.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ DeviceCenter/DeviceCenter/ViewDevicesPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Tools.Connectivity;
+
+namespace DeviceCenter
+{
+    /// <summary>
+    /// Interaction logic for ViewDevicesPage.xaml
+    /// </summary>
+    public partial class ViewDevicesPage : Page
+    {
+        public ViewDevicesPage()
+        {
+            InitializeComponent();
+            var deviceDiscoverySvc = new DeviceDiscoveryService();
+            deviceDiscoverySvc.Discovered += MDNSDeviceDiscovered;
+            deviceDiscoverySvc.Start();
+        }
+
+        public void MDNSDeviceDiscovered(object sender, DiscoveredEventArgs args)
+        {
+            // EventArgs args should never be null, added a check just to be sure. 
+            if (args == null)
+            {
+                return;
+            }
+
+            if (args.Info.Connection == DiscoveredDeviceInfo.ConnectionType.MDNS)
+            {
+                var newDevice = new mDNSDiscoveredDevice
+                {
+                    DeviceName = args.Info.Name,
+                    DeviceModel = args.Info.Location,
+                    Architecture = args.Info.Architecture,
+                    OSVersion = args.Info.OSVersion,
+                    IPaddress = args.Info.Address
+                };
+
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => discoveredDevices.Items.Add(newDevice)));
+            }
+        }
+
+        class mDNSDiscoveredDevice
+        {
+            public string DeviceName { get; set; }
+            public string DeviceModel { get; set; }
+            public string IPaddress { get; set; }
+            public string OSVersion { get; set; }
+            public string Architecture { get; set; }
+
+
+        }
+    }
+}
