07bd236da1433d1a750354849a42e45fa83deed8
tenglu83 <tenglu@MININT-T7FA6SG.redmond.corp.microsoft.com>
2015-10-08 11:04:16 -0700

    Add WebBRest APIs

--- DeviceCenter/DeviceCenter/PageTestOnly.xaml.cs
+++ DeviceCenter/DeviceCenter/PageTestOnly.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -25,9 +26,12 @@ namespace DeviceCenter
             InitializeComponent();
         }
 
-        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        private async void buttonTest_Click(object sender, RoutedEventArgs e)
         {
             // do something here
+            IPAddress ip = IPAddress.Parse("10.125.152.67");
+            WebBRest webbRequest = new WebBRest(ip, "Administrator", "p@ssw0rd");
+            await webbRequest.SetDeviceNameAsync("Wohaha");
         }
     }
 }
---
+++ DeviceCenter/DeviceCenter/WebBRest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace DeviceCenter
+{
+    public class WebBRest
+    {
+        public static IPAddress IpAddr;
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public static string Port { get; set; } = "8080";
+        public static string Admin { get; } = "Administrator";
+        public static string AdminPwd { get; set; } = "p@ssw0rd";
+        private static string DeviceApiUrl { get; } = "/api/iot/device/";
+        private static string AppxApiUrl { get; } = "/api/appx/packagemanager/";
+        private static string HttpUrlPrfx { get; } = "http://";
+        public WebBRest(IPAddress ip, string username, string password)
+        {
+            IpAddr = ip;
+            Username = username;
+            Password = password;
+        }
+
+        public async Task<bool> SetDeviceNameAsync(string newDeviceName)
+        {
+            string url = HttpUrlPrfx + IpAddr.ToString() + ":" + Port + DeviceApiUrl + "name?" + newDeviceName;
+            try
+            {
+                await PostRequest(url);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        //public async void SetPasswordAsync(string oldPassword, string newPassword)
+        //{
+        //    string url = HttpUrlPrfx + IpAddr.ToString() + ";" + Port + DeviceApiUrl "?"
+        //}
+
+        //public async void InstallAppx()
+        //{
+
+        //}
+
+        private async Task<HttpStatusCode> PostRequest(string url)
+        {
+            Stream objStream = null;
+            StreamReader objReader = null;
+            Debug.WriteLine(url);
+            HttpStatusCode result = HttpStatusCode.BadRequest;
+
+            try
+            {
+                HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+                req.Method = "POST";
+                req.Credentials = new NetworkCredential(Username, Password);
+
+                HttpWebResponse response = (HttpWebResponse)(await req.GetResponseAsync());
+                result = response.StatusCode;
+                if (result == HttpStatusCode.OK)
+                {
+                    objStream = response.GetResponseStream();
+                    objReader = new StreamReader(objStream);
+                    string respData = objReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+    }
+}
