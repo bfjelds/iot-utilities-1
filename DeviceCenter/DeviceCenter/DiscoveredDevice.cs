f90342d9a35f03c0f3ae620b7f796670eb289a76
brendanrempel <brempel@microsoft.com>
2015-10-08 18:33:23 -0700

    add new files missing from last checkin

---
+++ DeviceCenter/DeviceCenter/DiscoveredDevice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DeviceCenter
+{
+    public class DiscoveredDevice
+    {
+        public enum NetworkType { ethernet, adhoc };
+        public DiscoveredDevice(NetworkType network)
+        {
+            this.Network = network;
+            switch (network)
+            {
+                case NetworkType.ethernet:
+                    this.ManageVisible = Visibility.Visible;
+                    this.ConnectVisible = Visibility.Collapsed;
+                    break;
+                case NetworkType.adhoc:
+                    this.ManageVisible = Visibility.Collapsed;
+                    this.ConnectVisible = Visibility.Visible;
+                    break;
+            }
+        }
+
+        public NetworkType Network { get; private set; }
+        public string DeviceName { get; set; }
+        public string DeviceModel { get; set; }
+        public string IPaddress { get; set; }
+        public string OSVersion { get; set; }
+        public string Architecture { get; set; }
+        public Guid UniqueId { get; set; }
+        public Uri Manage { get; set; }
+        public Visibility ManageVisible { get; private set; }
+        public Visibility ConnectVisible { get; private set; }
+    }
+}
---
+++ DeviceCenter/DeviceCenter/PageDeviceConfiguration.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeviceCenter
+{
+    /// <summary>
+    /// Interaction logic for PageDeviceConfiguration.xaml
+    /// </summary>
+    public partial class PageDeviceConfiguration : Page
+    {
+        public DiscoveredDevice Device { get; private set; }
+        private Frame _navigationFrame;
+
+        public PageDeviceConfiguration(Frame navigationFrame, DiscoveredDevice device)
+        {
+            InitializeComponent();
+
+            this._navigationFrame = navigationFrame;
+            this.Device = device;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LabelDeviceName.Text = this.Device.DeviceName;
+            textBoxDeviceName.Text = this.Device.DeviceName;
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationFrame.GoBack();
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            //textBoxDeviceName.Text;
+            //textBoxCurrentPassword.Text
+            //textBoxPassword1
+
+            // do stuff here
+        }
+    }
+}
