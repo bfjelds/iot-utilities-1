8d235261b6c2dfcc2db07ebdb2b22a855e715126
brendanrempel <brempel@microsoft.com>
2015-10-19 16:09:58 -0700

    Fixed merge conflict

--- DeviceCenter/DeviceCenter/SamplesPage.xaml.cs
+++ DeviceCenter/DeviceCenter/SamplesPage.xaml.cs
@@ -19,11 +19,6 @@ namespace DeviceCenter
 
             App.TelemetryClient.TrackPageView(this.GetType().Name);
 
-            this.AppList.Add(new AppInformation(
-                "Assets/Blinky.png",
-                Strings.Strings.SamplesBlinkyTitle,
-                Strings.Strings.SamplesBlinkyMessage1 + "\n" + Strings.Strings.SamplesBlinkyMessage2));
-
             foreach (AppInformation cur in AppInformation.Initialize())
             {
                 ButtonAppInfo newButton = new ButtonAppInfo(cur);
