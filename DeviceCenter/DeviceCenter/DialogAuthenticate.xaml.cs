d6d3e4e568a4c0e671dff5e733fe665c0f3c588d
brendanrempel <brempel@microsoft.com>
2015-10-20 16:18:46 -0700

    Add authentication dialog

---
+++ DeviceCenter/DeviceCenter/DialogAuthenticate.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceCenter
+{
+    using System;
+    using System.Windows;
+    /// <summary>
+    /// Interaction logic for DialogAuthenticate.xaml
+    /// </summary>
+    public partial class DialogAuthenticate : Window
+    {
+        public DialogAuthenticate()
+        {
+            InitializeComponent();
+
+            //EditUserName.Place
+            //EditPassword.Placeholder
+        }
+
+
+    }
+}
