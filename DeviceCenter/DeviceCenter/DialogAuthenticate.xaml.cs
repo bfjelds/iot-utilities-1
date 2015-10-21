0518886cfbcf7f0b7f9bb4643f6f1cfd7ea2009b
brendanrempel <brempel@microsoft.com>
2015-10-20 18:11:38 -0700

    Add authentication dialog

--- DeviceCenter/DeviceCenter/DialogAuthenticate.xaml.cs
+++ DeviceCenter/DeviceCenter/DialogAuthenticate.xaml.cs
@@ -1,26 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeviceCenter
+﻿namespace DeviceCenter
 {
     using System;
     using System.Windows;
+    public class UserInfo
+    {
+        public UserInfo()
+        {
+            this.DeviceName = string.Empty;
+            this.UserName = string.Empty;
+            this.Password = string.Empty;
+            this.SavePassword = false;
+        }
+
+        public string DeviceName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool? SavePassword { get; set; }
+    }
+
+
     /// <summary>
     /// Interaction logic for DialogAuthenticate.xaml
     /// </summary>
     public partial class DialogAuthenticate : Window
     {
-        public DialogAuthenticate()
+        public DialogAuthenticate(UserInfo info)
         {
             InitializeComponent();
 
-            //EditUserName.Place
-            //EditPassword.Placeholder
+            DataContext = info;
         }
 
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            NativeMethods.HideSystemMenu(this);
+        }
 
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            (this.DataContext as UserInfo).UserName = editUserName.Text;
+            (this.DataContext as UserInfo).Password = editPassword.Password;
+            (this.DataContext as UserInfo).SavePassword = checkboxSavePassword.IsChecked;
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void UpdateState()
+        {
+            buttonOk.IsEnabled =
+                editPassword.Password.Length > 0 &&
+                editUserName.Length > 0;
+        }
+
+        private void editPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateState();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateState();
+        }
     }
 }
