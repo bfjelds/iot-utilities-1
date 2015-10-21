e36f1f010781cf970c050e65c03148f89f52e031
tenglu83 <tenglu@MININT-T7FA6SG.redmond.corp.microsoft.com>
2015-10-21 15:06:13 -0700

    Finish appx deployment.

--- DeviceCenter/DeviceCenter/PageTestOnly.xaml.cs
+++ DeviceCenter/DeviceCenter/PageTestOnly.xaml.cs
@@ -29,14 +29,16 @@ namespace DeviceCenter
         private async void buttonTest_Click(object sender, RoutedEventArgs e)
         {
             // do something here
-            IPAddress ip = IPAddress.Parse("10.125.140.92");
+            IPAddress ip = IPAddress.Parse("10.125.140.161");
             WebBRest webbRequest = new WebBRest(ip, "Administrator", "p@ssw0rd");
             //bool x = await webbRequest.SetDeviceNameAsync("tengtestWohaha");
             //bool y = await webbRequest.SetPasswordAsync("p@ssw0rd", "password1");
             //bool z = await webbRequest.Restart();
-            string[] files = new String[2];
-            files[0] = @"BasicDepthUAP.appx";
-            files[1] = @"testroot-sha2.cer";
+            string[] files = new String[4];
+            files[0] = @"InternetRadioHeaded_1.0.0.0_ARM.appx";
+            files[1] = @"InternetRadioHeaded_1.0.0.0_ARM.cer";
+            files[2] = @"Microsoft.NET.Native.Runtime.1.1.appx";
+            files[3] = @"Microsoft.VCLibs.ARM.14.00.appx";
             bool x = await webbRequest.InstallAppxAsync(files);
         }
     }
