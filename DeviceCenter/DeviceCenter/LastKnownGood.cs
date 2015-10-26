3362b565ac229bfa27fc6a7b33ecda29de677e95
Soemin Tjong <stjong@microsoft.com>
2015-10-26 10:50:13 -0700

    fix build break.

--- DeviceCenter/DeviceCenter/LastKnownGood.cs
+++ DeviceCenter/DeviceCenter/LastKnownGood.cs
@@ -90,7 +90,7 @@ namespace DeviceCenter
         /// <summary>
         /// LKG info file.  tbd point to the final file.
         /// </summary>
-        static string LKGFileName = "\\\\webnas\\AthensDrop\\LKG\\iot_lkg.txt";
+        static string LkgFileName = "\\\\webnas\\AthensDrop\\LKG\\iot_lkg.txt";
         
         // tbd - remove this.
         // static readonly string LkgFileName = "c:\\temp\\iot_lkg.txt";
