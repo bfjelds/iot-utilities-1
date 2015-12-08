b05cacad9765255550b7bdbdf008c82c2529886b
Rachit Bajpai <rachitb@microsoft.com>
2015-12-08 15:47:08 -0800

    Enable SN signing

--- DeviceCenter/DeviceCenter/Properties/AssemblyInfo.cs
+++ DeviceCenter/DeviceCenter/Properties/AssemblyInfo.cs
@@ -15,6 +15,8 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyKeyFileAttribute("35MSSharedLib1024.snk")]
+[assembly: AssemblyDelaySignAttribute(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
